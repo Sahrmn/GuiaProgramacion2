0f96797059b93558d70f1fa22a2fef57b9d1511d
Sahrmn <samy32m@gmail.com>
2018-04-11 21:35:05 -0300

    Ejercicio 19

---
+++ Ejercicio19/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ejercicio19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sumador suma = new Sumador();
+            //Console.WriteLine(suma.Sumar("hola","mundo"));
+            //Console.WriteLine(suma.Sumar(4,8));
+
+            Sumador sumita = new Sumador(14);
+            Sumador sumita2 = new Sumador(14);
+            Console.WriteLine(sumita + sumita2);
+            Console.WriteLine(sumita|sumita2);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Ejercicio19/Sumador.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio19
+{
+    class Sumador
+    {
+        private int _cantidadSumas;
+
+        public Sumador(int cant)
+        {
+            this._cantidadSumas = cant;
+        }
+
+        public Sumador() : this(0)
+        { }
+
+        public long Sumar(long a, long b)
+        {
+            long retValue = a + b;
+            this._cantidadSumas++;
+            return retValue;
+        }
+
+        public string Sumar(string a, string b)
+        {
+            string retValue = a + b;
+            this._cantidadSumas++;
+            return retValue;
+        }
+
+        public static explicit operator int(Sumador s)
+        {
+            return s._cantidadSumas;
+        }
+
+        public static long operator +(Sumador s1, Sumador s2)
+        {
+            long retValue = (int)s1 + (int)s2;
+            return retValue;
+        }
+
+        public static bool operator |(Sumador s1, Sumador s2)
+        {
+            bool retValue = false;
+            if ((int)s1 == (int)s2)
+            {
+                retValue = true;
+            }
+            return retValue;
+        }
+    }
+}
