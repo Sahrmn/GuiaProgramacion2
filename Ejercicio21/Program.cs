1825821bdbf3e837e06ea719e888aaab235b770b
Sahrmn <samy32m@gmail.com>
2018-04-15 19:55:41 -0300

    ejercicio 21

---
+++ Ejercicio21/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ejercicio21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Celcius cel = new Celcius(1);
+            Fahrenheit fah = new Fahrenheit(1);
+            Kelvin kel = new Kelvin(1);
+
+            Fahrenheit prueba1 = (Fahrenheit)cel;
+            Console.WriteLine(prueba1.GetCantidad());
+            Kelvin prueba2 = (Kelvin)cel;
+            Console.WriteLine(prueba2.GetCantidad());
+
+            Console.WriteLine("\n");
+
+            Celcius prueba3 = (Celcius)fah;
+            Console.WriteLine(prueba3.GetCantidad());
+            prueba2 = (Kelvin)fah;
+            Console.WriteLine(prueba2.GetCantidad());
+
+            Console.WriteLine("\n");
+
+            prueba3 = (Celcius)kel;
+            Console.WriteLine(prueba3.GetCantidad());
+            prueba1 = (Fahrenheit)kel;
+            Console.WriteLine(prueba1.GetCantidad());
+            Console.ReadLine();
+
+        }
+    }
+}
