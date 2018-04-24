41880164c544ebae40e621a8da2169a1fda260d6
Sahrmn <samy32m@gmail.com>
2018-04-23 23:03:12 -0300

    ejercicio 27 -> no terminado

---
+++ Ejercicio28/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Ejercicio28
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPalabras = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtPalabras
+            // 
+            this.txtPalabras.Location = new System.Drawing.Point(1, 1);
+            this.txtPalabras.Multiline = true;
+            this.txtPalabras.Name = "txtPalabras";
+            this.txtPalabras.Size = new System.Drawing.Size(529, 295);
+            this.txtPalabras.TabIndex = 0;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(444, 302);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 341);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtPalabras);
+            this.Name = "Form1";
+            this.Text = "Contador de palabras";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPalabras;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ Ejercicio28/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio28
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, int> diccionario;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            diccionario = new Dictionary<string, int>();
+            string palabras = txtPalabras.Text;
+            string[] palabrasSeparadas = palabras.Split(' ');
+            int count = 0;
+            
+            foreach (string item in palabrasSeparadas)
+            {
+                if (diccionario.ContainsKey(item) != true)
+                {
+                    count = 1;
+                    diccionario.Add(item, count);
+                }                  else
+                {
+                    if (diccionario.TryGetValue(item, out int key)) //busca la key de la palabra, si existe devuelve el valor entero que lo acompaña
+                    {
+                        diccionario[item] = key + 1;
+                    }
+                    else
+                        MessageBox.Show("Hubo un error");
+                }
+            }
+
+            //SortedDictionary<string, int> ordenado = new SortedDictionary<string, int>(diccionario);
+
+            var ordenado = from elemento in diccionario orderby elemento.Value descending select elemento;
+
+            string mostrar = "";
+            int contador = 0;
+            foreach (object item in ordenado)
+            {
+                if (contador < 3)
+                {
+                    mostrar += item + "\n";
+                    contador++;
+                }
+                else
+                    break;
+            }
+
+            MessageBox.Show(mostrar);
+
+            //MessageBox.Show("Realizado!");
+            
+
+        }
+    }
+}
